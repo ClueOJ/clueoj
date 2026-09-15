a5ddd03e0f7342adc697987ddc6d9020323175c8
phancddev <phancddev@gmail.com>
2026-09-15 19:17:34 +0700

    test: grant edit_own_problem to data page editor fixture

--- repo/judge/tests/test_storage.py
+++ repo/judge/tests/test_storage.py
@@ -1996,7 +1996,7 @@ class StorageAdminViewTestCase(TestCase):
 class ProblemDataArchivedTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.editor = create_user('data_editor', user_permissions=['edit_all_problem'])
+        cls.editor = create_user('data_editor', user_permissions=['edit_own_problem', 'edit_all_problem'])
         cls.problem = create_problem('archived_prob')
 
     def setUp(self):
